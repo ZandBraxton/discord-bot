56229754231f152d2903ccd3b4967d6501319948
Braxton Zand <Zand.Braxton@gmail.com>
2022-04-05 17:36:51 -0500

    Changec connection string

--- database.js
+++ database.js
@@ -1,11 +1,10 @@
 const { Pool, Client } = require("pg");
 require("dotenv").config();
 const pool = new Pool({
-  user: process.env.USER,
-  password: process.env.PASS,
-  host: process.env.HOST,
-  port: process.env.PORT,
-  database: process.env.DATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = {
