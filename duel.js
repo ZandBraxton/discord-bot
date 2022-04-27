1e85991a2896908723b88f53a50e97d791977d22
Braxton Zand <Zand.Braxton@gmail.com>
2022-04-27 13:40:48 -0500

    Patch 1.3.5

--- duel.js
+++ duel.js
@@ -49,36 +49,72 @@ async function attack(attacker, defender, message) {
   defender.evadeChance += 2;
 }
 
-function getRandomItem(max, rarity) {
+function getRandomItem(max, rarity, Player) {
   let index = randomInt(max);
-  return duelItems.weapons[rarity][index];
+  if (rarity === "ULTIMATE") {
+    let p1Weapons = duelItems.weapons["ULTIMATE"].filter((weapon) =>
+      weapon.special["trigger-name"].includes(Player.name)
+    );
+    if (p1Weapons.length !== 0) {
+      index = randomInt(p1Weapons.length);
+      return p1Weapons[index];
+    } else {
+      return duelItems.weapons["ULTIMATE"][index];
+    }
+  } else {
+    return duelItems.weapons[rarity][index];
+  }
 }
 
-function getRandomEvent(max) {
+function getRandomEvent(max, type, P1, P2) {
   let index = randomInt(max);
-  return duelItems.events[index];
+  switch (type) {
+    case "standard":
+      return duelItems.events[index];
+      break;
+    case "ultimate":
+      let p1array = duelItems["Ultimate Events"].filter((event) =>
+        event["trigger-name"].includes(P1.name)
+      );
+
+      let p2array = duelItems["Ultimate Events"].filter((event) =>
+        event["trigger-name"].includes(P2.name)
+      );
+
+      if (p1array.length !== 0 && p2array.length !== 0) {
+        let joinArray = p1array.concat(p2array);
+        index = randomInt(joinArray.length);
+        return joinArray[index];
+      } else if (p1array.length !== 0) {
+        index = randomInt(p1array.length);
+        return p1array[index];
+      } else if (p2array.length !== 0) {
+        index = randomInt(p2array.length);
+        return p2array[index];
+      } else {
+        index = randomInt(duelItems["Ultimate Events"].length);
+        return duelItems["Ultimate Events"][index];
+      }
+      break;
+    default:
+      break;
+  }
 }
 
 function getWeapon(Player, message) {
   if (Player.weapon === "") {
-    let rarity = randomInt(100) + 1;
-    if (rarity <= 50) {
-      rarity = "Common";
-    } else if (rarity > 51 && rarity <= 80) {
-      rarity = "Rare";
+    let ultimateCheck = randomInt(100) + 1;
+    if (ultimateCheck <= 10) {
+      let item = getRandomItem(
+        duelItems.weapons["ULTIMATE"].length,
+        "ULTIMATE",
+        Player
+      );
+      Player.weapon = item;
+      message.channel.send(
+        `**${Player.name}** picked up a ${Player.weapon.name} **[ULTIMATE]**`
+      );
     } else {
-      rarity = "Legendary";
-    }
-    let item = getRandomItem(duelItems.weapons[rarity].length, rarity);
-    Player.weapon = item;
-    message.channel.send(
-      `**${Player.name}** picked up a ${Player.weapon.name} **[${rarity}]**`
-    );
-  }
-
-  if (Player.weapon.name === "Fists") {
-    let roll = randomInt(100) + 1;
-    if (roll < 40) {
       let rarity = randomInt(100) + 1;
       if (rarity <= 50) {
         rarity = "Common";
@@ -87,11 +123,47 @@ function getWeapon(Player, message) {
       } else {
         rarity = "Legendary";
       }
-      item = getRandomItem(duelItems.weapons[rarity].length, rarity);
+      let item = getRandomItem(
+        duelItems.weapons[rarity].length,
+        rarity,
+        Player
+      );
       Player.weapon = item;
       message.channel.send(
         `**${Player.name}** picked up a ${Player.weapon.name} **[${rarity}]**`
       );
+    }
+  }
+
+  if (Player.weapon.name === "Fists") {
+    let roll = randomInt(100) + 1;
+    if (roll < 40) {
+      let ultimateCheck = randomInt(100) + 1;
+      if (ultimateCheck <= 15) {
+        let item = getRandomItem(
+          duelItems.weapons["ULTIMATE"].length,
+          "ULTIMATE",
+          Player
+        );
+        Player.weapon = item;
+        message.channel.send(
+          `**${Player.name}** picked up a ${Player.weapon.name} **[ULTIMATE]**`
+        );
+      } else {
+        let rarity = randomInt(100) + 1;
+        if (rarity <= 40) {
+          rarity = "Common";
+        } else if (rarity > 41 && rarity <= 75) {
+          rarity = "Rare";
+        } else {
+          rarity = "Legendary";
+        }
+        item = getRandomItem(duelItems.weapons[rarity].length, rarity, Player);
+        Player.weapon = item;
+        message.channel.send(
+          `**${Player.name}** picked up a ${Player.weapon.name} **[${rarity}]**`
+        );
+      }
     }
     return;
   }
@@ -332,7 +404,7 @@ async function gameOver(winner, message, db, p1, p2, amount) {
 
 function getHP(amount) {
   if (amount <= 49) {
-    return 15;
+    return 20;
   } else if (amount >= 50 && amount <= 299) {
     return 25;
   } else if (amount >= 300 && amount <= 499) {
@@ -344,17 +416,89 @@ function getHP(amount) {
   }
 }
 
-function randomEvent(Player1, Player2, message, db, p1, p2, amount, duelCheck) {
-  let event = getRandomEvent(duelItems.events.length);
-  message.channel.send(`**!!! EVENT !!!**`);
-  setTimeout(() => {
-    message.channel.send(`${event.message}`);
-    message.channel.send(`${event.attack}`);
+function randomEvent(
+  Player1,
+  Player2,
+  message,
+  client,
+  p1,
+  p2,
+  amount,
+  duelCheck
+) {
+  //ultimate check
+
+  let ultimateCheck = randomInt(100) + 1;
+  if (ultimateCheck <= 10) {
+    let event = getRandomEvent(
+      duelItems["Ultimate Events"].length,
+      "ultimate",
+      Player1,
+      Player2
+    );
+    message.channel.send(`**!!! ULTIMATE EVENT !!!**`);
+
+    switch (event["trigger-name"]) {
+      case Player1.name:
+        setTimeout(() => {
+          message.channel.send(`${event["trigger-event"].message}`);
+          message.channel.send(`${event["trigger-event"].attack}`);
+          setTimeout(() => {
+            getUltimateEventResult(
+              event["trigger-event"],
+              Player1,
+              Player2,
+              message
+            );
+            Round(Player1, Player2, message, client, p1, p2, amount, duelCheck);
+          }, 2000);
+        }, 1000);
+        break;
+
+      case Player2.name:
+        setTimeout(() => {
+          message.channel.send(`${event["trigger-event"].message}`);
+          message.channel.send(`${event["trigger-event"].attack}`);
+          setTimeout(() => {
+            getUltimateEventResult(
+              event["trigger-event"],
+              Player2,
+              Player1,
+              message
+            );
+            Round(Player1, Player2, message, client, p1, p2, amount, duelCheck);
+          }, 2000);
+        }, 1000);
+        break;
+
+      default:
+        setTimeout(() => {
+          message.channel.send(`${event.message}`);
+          message.channel.send(`${event.attack}`);
+          setTimeout(() => {
+            getEventResult(event, Player1, Player2, message);
+            Round(Player1, Player2, message, client, p1, p2, amount, duelCheck);
+          }, 2000);
+        }, 1000);
+        break;
+    }
+  } else {
+    let event = getRandomEvent(
+      duelItems.events.length,
+      "standard",
+      Player1,
+      Player2
+    );
+    message.channel.send(`**!!! EVENT !!!**`);
     setTimeout(() => {
-      getEventResult(event, Player1, Player2, message);
-      Round(Player1, Player2, message, db, p1, p2, amount, duelCheck);
-    }, 2000);
-  }, 1000);
+      message.channel.send(`${event.message}`);
+      message.channel.send(`${event.attack}`);
+      setTimeout(() => {
+        getEventResult(event, Player1, Player2, message);
+        Round(Player1, Player2, message, client, p1, p2, amount, duelCheck);
+      }, 2000);
+    }, 1000);
+  }
 }
 
 function getEventResult(event, attacker, defender, message) {
@@ -382,6 +526,17 @@ function getEventResult(event, attacker, defender, message) {
     }
   } else {
     message.channel.send(`${event.miss}`);
+  }
+}
+
+function getUltimateEventResult(event, attacker, defender, message) {
+  let strike = calculateDamage(event.minDamage, event.maxDamage, null);
+  if (event.type === "heal") {
+    message.channel.send(`${attacker.name} was healed for ${strike} hp!`);
+    attacker.hp += strike;
+  } else {
+    message.channel.send(`${defender.name} takes ${strike} damage!`);
+    defender.hp -= strike;
   }
 }
 
